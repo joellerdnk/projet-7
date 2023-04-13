859521702c1f004192d057ed7a7be1b54634a210
Joëlle Rudnik <joelle_rudnik@hotmail.fr>
2023-04-13 16:15:27 +0200

    final

---
+++ src/components/Carrousel/Carrousel.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react"
+import left from "../../assets/images/vector-left.svg"
+import right from "../../assets/images/vector-right.svg"
+
+export default function Carrousel({ slides }) {
+	// Définir l'index du premier slide à 0
+	const [current, setCurrent] = useState(0)
+	const length = slides.length
+
+	const nextSlide = () => {
+		// On repart au premier slide quand on arrive au dernier
+		setCurrent(current === length - 1 ? 0 : current + 1)
+	}
+	const prevSlide = () => {
+		// On repart au dernier slide quand on est au premier
+		setCurrent(current === 0 ? length - 1 : current - 1)
+	}
+
+	return (
+		<section id="carrousel-container">
+			{length > 1 && (
+				<img
+					src={left}
+					alt="gauche"
+					onClick={prevSlide}
+					className="leftArrow"
+				/>
+			)}
+			{length > 1 && (
+				<img
+					src={right}
+					alt="droite"
+					onClick={nextSlide}
+					className="rightArrow"
+				/>
+			)}
+			{slides.map((slide, index) => (
+				<div
+					key={index}
+					className={
+						current === index
+							? "slider bl-msk wh-msk active-anim"
+							: "slider bl-msk wh-msk"
+					}
+				>
+					{index === current && <img src={slide} alt="appartement à louer" />}
+					{index === current && (
+						<span className="slider__number">
+							{current + 1}/{length}
+						</span>
+					)}
+				</div>
+			))}
+		</section>
+	)
+}
---
+++ src/components/Collapse/Collapse.jsx
@@ -0,0 +1,40 @@
+import { useState, useRef, useEffect } from "react"
+import Chevron from "../../assets/images/vectorBas.svg"
+
+export default function Collapse(props) {
+	// Définir le state du toggle (et false par défaut)
+	const [toggle, setToggle] = useState(false)
+	const [heightEl, setHeightEl] = useState()
+
+	const toggleState = () => {
+		// Définir la fonction toggleState qui modifie la valeur toggle au clic
+		setToggle(!toggle)
+	}
+	// Récupération et conservation de la valeur de hauteur du collapse déplié
+	const refHeight = useRef()
+
+	useEffect(() => {
+		setHeightEl(`${refHeight.current.scrollHeight}px`)
+	}, [])
+
+	return (
+		// Affichage le collapse replié par défaut et l'ouvre au clic puis le referme au clic en faisant disparaitre le texte et le style
+		<div className={`collapse ${props.aboutStyle}`}>
+			<div onClick={toggleState} className="collapse__visible">
+				<h2>{props.aboutTitle}</h2>
+				<img
+					className={toggle ? "chevron rotated" : "chevron"}
+					src={Chevron}
+					alt="chevron"
+				/>
+			</div>
+			<div
+				ref={refHeight}
+				className={toggle ? "collapse__toggle animated" : "collapse__toggle"}
+				style={{ height: toggle ? `${heightEl}` : "0px" }}
+			>
+				<p aria-hidden={toggle ? "true" : "false"}>{props.aboutText}</p>
+			</div>
+		</div>
+	)
+}
