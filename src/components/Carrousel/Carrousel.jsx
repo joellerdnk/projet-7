7d7934639fe131138d37c2cd8e7672d8caaa5399
Joëlle Rudnik <joelle_rudnik@hotmail.fr>
2023-04-24 15:30:00 +0200

    final

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import left from "../../assets/images/vector-left.svg"
 import right from "../../assets/images/vector-right.svg"
 
-export default function Carrousel({ slides }) {
+export default function Carrousel({ slides, index }) {
 	// Définir l'index du premier slide à 0
 	const [current, setCurrent] = useState(0)
 	const length = slides.length
@@ -16,8 +16,10 @@ export default function Carrousel({ slides }) {
 		setCurrent(current === 0 ? length - 1 : current - 1)
 	}
 
+
 	return (
 		<section id="carrousel-container">
+			{/* S'il y a plus d'une image, affichage des flèches */}
 			{length > 1 && (
 				<img
 					src={left}
@@ -34,17 +36,20 @@ export default function Carrousel({ slides }) {
 					className="rightArrow"
 				/>
 			)}
+
 			{slides.map((slide, index) => (
 				<div
 					key={index}
 					className={
-						current === index
+						index === current
 							? "slider bl-msk wh-msk active-anim"
 							: "slider bl-msk wh-msk"
 					}
 				>
 					{index === current && <img src={slide} alt="appartement à louer" />}
-					{index === current && (
+					
+					{/* S'il y a plus d'une image, affichage du compteur */}
+					{length > 1 &&  (
 						<span className="slider__number">
 							{current + 1}/{length}
 						</span>
@@ -53,4 +58,4 @@ export default function Carrousel({ slides }) {
 			))}
 		</section>
 	)
-}
+} 
